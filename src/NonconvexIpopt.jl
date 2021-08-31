a931528fec46d3150b114ab8984e29301a84dc5c
Mohamed Tarek <mohamed82008@gmail.com>
2021-08-31 17:26:39 +1000

    move code from Nonconvex

--- src/NonconvexIpopt.jl
+++ src/NonconvexIpopt.jl
@@ -1,5 +1,14 @@
 module NonconvexIpopt
 
-# Write your package code here.
+export IpoptAlg, IpoptOptions
+
+using Reexport, Parameters, SparseArrays, Zygote
+@reexport using NonconvexCore
+using NonconvexCore: @params, VecModel, AbstractResult
+using NonconvexCore: AbstractOptimizer, CountingFunction
+import NonconvexCore: optimize!
+using Ipopt
+
+include("ipopt.jl")
 
 end
